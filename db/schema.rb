098f4156a22e36ef59087b9e3a307fe296abf494
Evelyn Frech <evelynmariafrech@gmail.com>
2022-09-08 10:46:38 -0400

    create migration to add title and description to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_08_141138) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_08_143944) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.text "description"
   end
 
 end
